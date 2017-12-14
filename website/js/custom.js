b7bacb644daaf450de555827ec0d35c4cdb13ea2
budi1200 <alenab22@gmail.com>
2017-12-14 20:51:13 +0100

    fancy++ underline

--- website/js/custom.js
+++ website/js/custom.js
@@ -1,4 +1,8 @@
 function buttonClick(id) {
-    $('a').css("color", '#74B1BE');
-    document.getElementById(id).style.color = "#20B2AA";
+    var al = $('a');
+    al.css("color", '#74B1BE');
+    al.css("border-bottom", "");
+    var el = document.getElementById(id);
+    el.style.color = "#20B2AA";
+    el.style.borderBottom = "2px solid lightseagreen";
 }
