17247d76002e4bd5dc4a3409c010f7f0599fd2da
budi1200 <alenab22@gmail.com>
2017-12-15 12:25:45 +0100

    vaja pages

--- website/js/custom.js
+++ website/js/custom.js
@@ -6,3 +6,11 @@ function buttonClick(id) {
     el.style.color = "#20B2AA";
     el.style.borderBottom = "2px solid lightseagreen";
 }
+
+function next(id){
+    var a = document.getElementById("tutorial1_" + id);
+    a.style.display = "none";
+    id++;
+    var b = document.getElementById("tutorial1_" + id);
+    b.style.display = "block";
+}
