d0c4c44fd6e6f2b4558b3f1bbc16fb38e46f2a31
masterdodo <dodo.arastava@gmail.com>
2017-12-19 08:45:27 +0100

    js beauty fix

--- website/js/custom.js
+++ website/js/custom.js
@@ -1,26 +1,31 @@
 var id = 1;
 
-function buttonClick(id) {
-    var al = $("#nav_buttons a");
-    al.css("color", '#74B1BE');
-    al.css("border-bottom", "");
-    var el = document.getElementById(id);
-    el.style.color = "#20B2AA";
-    el.style.borderBottom = "2px solid lightseagreen";
-}
-
-function next(){
-    var a = document.getElementById("tutorial1_" + id);
-    a.style.display = "none";
-    id++;
-    var b = document.getElementById("tutorial1_" + id);
-
-    if(b == null){
-        console.log('null');
-        $('#button_wrap').css('display', 'none');
-        $('#button_naprej').css('display', 'block');
-        id=1;
-    }else{
-        b.style.display = "block";
+function buttonClick(id)
+    {
+        var al = $("#nav_buttons a");
+        al.css("color", '#74B1BE');
+        al.css("border-bottom", "");
+        var el = document.getElementById(id);
+        el.style.color = "#20B2AA";
+        el.style.borderBottom = "2px solid lightseagreen";
     }
-}
+
+function next()
+    {
+        var a = document.getElementById("tutorial1_" + id);
+        a.style.display = "none";
+        id++;
+        var b = document.getElementById("tutorial1_" + id);
+
+        if(b == null)
+        {
+            console.log('null');
+            $('#button_wrap').css('display', 'none');
+            $('#button_naprej').css('display', 'block');
+            id=1;
+        }
+        else
+        {
+            b.style.display = "block";
+        }
+    }
